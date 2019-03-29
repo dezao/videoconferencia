9e7876bc2ec70ff1cf1fef062ee1d13a6dacc7cc
André Cunha de Oliveira <acoliveira87@gmail.com>
2019-03-29 15:24:30 -0300

    Correções na configurações de visualização do painel

--- app/view/painel.php
+++ app/view/painel.php
@@ -54,9 +54,7 @@
 
     }
 
-
     // configuração semana 
-
     switch ($semana) {
         case 0: $semana = "DOM"; break;
         case 1: $semana = "SEG"; break;
@@ -68,27 +66,26 @@
     }
 
 ?>
-    <script>
-        function horas() {
-            camada = document.getElementById('relogio');
-            hoje = new Date();
-            hora = hoje.getHours();
-            if(hora<=9){
-                hora = "0" + hora;
-            }
-            minuto = hoje.getMinutes();
-            if(minuto<=9) {
-                minuto = "0" + minuto;
-            }
-            segundo = hoje.getSeconds();
-            if(segundo<=9) {
-                segundo = "0" + segundo;
-            }
-            camada.innerHTML = hora + ":" + minuto;
-            setTimeout("horas()",1000);
+<script>
+    function horas() {
+        camada = document.getElementById('relogio');
+        hoje = new Date();
+        hora = hoje.getHours();
+        if(hora<=9){
+            hora = "0" + hora;
         }
-    </script>
-
+        minuto = hoje.getMinutes();
+        if(minuto<=9) {
+            minuto = "0" + minuto;
+        }
+        segundo = hoje.getSeconds();
+        if(segundo<=9) {
+            segundo = "0" + segundo;
+        }
+        camada.innerHTML = hora + ":" + minuto;
+        setTimeout("horas()",1000);
+    }
+</script>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -108,103 +105,88 @@
 </head>
 <body onload="horas()">
 
-
-        <div class="row">
-            <div class="col s12 m12">
-            <id id="alerta"></div>
-            <div class="card-panel #880e4f pink darken-4 white-text flow-text center-align s12"><img src="../../assets/img/logo_raizen.png"><h5>PAINEL DE VIDEOCONFERÊNCIAS</h5></div>
-            <div id="data"> 
-                <?php 
-                    // $date = date('Y-m-d');
-                    // echo strtoupper(strftime("%a | %d/%m", strtotime($date))); 
-                    echo ("$semana | $dia/$mes");
-                ?> 
-            </div>
-            <div id="relogio"></div>
+<div class="row">
+    <div class="card-panel #880e4f pink darken-4 white-text col s12 m12">
+        <br>
+        <div class="col s12 m4 l2 left-align"><h4><?php echo ("$semana | $dia/$mes"); ?></h4></div>
+        <div class="col s12 m4 l8 center-align"><img src="../../assets/img/logo_raizen.png"><h5>PAINEL DE VIDEOCONFERÊNCIAS<br></h5></div>
+        <h3><div class=" col s12 m4 l2 right-align" id="relogio"></div></h3>
+        <div class="row"></div>
+    </div>
+           
+    <table class="responsive-table centered bordered">
+        <thead class="responsive-table centered #880e4f pink darken-4">
+            <tr>
+                <th hidden>ID</th>
+                <th class="flow-text">INÍCIO</th>
+                <th class="flow-text">FIM</th>
+                <th class="flow-text">TICKET</th>
+                <th class="flow-text">PIN</th>
+                <th class="flow-text">SALAS FÍSICAS</th>
+            </tr>
+        </thead>
             
-            <table class="responsive-table centered bordered">
-                    <thead class="responsive-table centered #880e4f pink darken-4">
-                        <tr>
-                            <th hidden>ID</th>
-                            <!-- <th class="flow-text">VIP</th> -->
-                            <!-- <th class="flow-text">DATA</th> -->
-                            <th class="flow-text">INÍCIO</th>
-                            <th class="flow-text">FIM</th>
-                            <!-- <th class="flow-text">PARTICIPANTES</th> -->
-                            <th class="flow-text">TICKET</th>
-                            <th class="flow-text">PIN</th>
-<!--                             <th class="flow-text">UNIDADES</th> -->
-                            <th class="flow-text">SALAS FÍSICAS</th>
-<!--                            <th class="flow-text">OWNER</th>-->
-                        </tr>
-                    </thead>
-                    
-                    <tbody>
-                        <?php
-                            $dataDoDia = date("Y/m/d");
-                            $sqlVideo = "SELECT * FROM videoconferencias WHERE dia = '$dataDoDia' AND horaInicio > CURRENT_TIME() AND excluido = 0 AND salasFisicas LIKE '%$usuarioUnidade%' ORDER BY dia, horaInicio LIMIT 6"; 
-                            $resultadoVideo = mysqli_query($connect, $sqlVideo);
-                            mysqli_close($connect);
+        <tbody>
+            <?php
+                $dataDoDia = date("Y/m/d");
+                $sqlVideo = "SELECT * FROM videoconferencias WHERE dia = '$dataDoDia' AND horaInicio > CURRENT_TIME() AND excluido = 0 AND salasFisicas LIKE '%$usuarioUnidade%' ORDER BY dia, horaInicio LIMIT 6"; 
+                $resultadoVideo = mysqli_query($connect, $sqlVideo);
+                mysqli_close($connect);
 
-                            if (mysqli_num_rows($resultadoVideo) > 0) {
+                if (mysqli_num_rows($resultadoVideo) > 0) {
 
-                                while($dadosVideo = mysqli_fetch_array($resultadoVideo)) {
+                    while($dadosVideo = mysqli_fetch_array($resultadoVideo)) {
 
-                                    $vip = $dadosVideo['vip'];
-                                    switch($vip) {
-                                        case 'SIM' : $cor = '#fc4d4d'; $corFonte = '#fff'; break;
-                                        case 'NÃO' : $cor = '';  $corFonte = '' ; break;
-                                    }
-                                    
-                                    
-                                    $inicio = date('H:i', strtotime('-15 minute', strtotime($dadosVideo['horaInicio'])));                                  
-                                    if ($inicio === date('H:i')) {
-                                        echo "<embed src='../../assets/wav/sweetalertsound.wav' width='1' height='1'>";
-                                        echo '<div id="alertaVideo"><p align="center">A PRÓXIMA VIDEOCONFERÊNCIA COMEÇA EM 15 MINUTOS!</p></div>';                                        
-                                    }
-                        ?>
-                            <tr style='background-color:<?php echo $cor;?>; color:<?php echo $corFonte;?>'>
-                                <td hidden ><?php echo $dadosVideo['id']; ?></td>
-                                <!-- <td class="flow-text"><?php /* echo $dadosVideo['vip']; */?></td> -->
-                                    <?php $dia = $dadosVideo['dia']; 
-                                        $dia = date('d/m', strtotime($dia));
-                                    ?>
-                               <!--  <b><td class="flow-text"><?php /* echo $dia; */ ?></td> -->
-                                <td class="flow-text"><?php echo date('H:i', strtotime($dadosVideo['horaInicio'])); ?></td>
-                                <td class="flow-text"><?php echo date('H:i', strtotime($dadosVideo['horaFim'])); ?></td></b>
-                                <!-- <td class="flow-text"><?php  /* echo strtoupper($dadosVideo['nomeParticipantes']); */ ?></td> -->
-                                <!-- <td class="flow-text"><?php /* echo $dadosVideo['unidadesParticipantes']; */ ?></td> -->
-                                <td class="flow-text"><?php echo $dadosVideo['ticket']; ?></td>
-                                <td class="flow-text"><?php echo $dadosVideo['pin']; ?></td>
-<!--                                <td class="flow-text"><?php /*echo $dadosVideo['unidadesParticipantes'];*/ ?></td>-->
-                                <td class="flow-text"><?php echo $dadosVideo['salasFisicas']; ?></td>
-<!--                                <td class="flow-text"><?php /*echo $dadosVideo['analistaResponsavel'];*/ ?></td> -->
-
-                            </tr>
-                        <?php 
-                        }  
-                            } else { ?>
-                                
-                                <tr>
-                                    <td colspan="12"><div class="card-panel red center-align s12 text-lighten-1"><span style="color: #fff;">NENHUM REGISTRO ENCONTRADO</span></div></td>
-                                </tr>
-                        <?php    }  
+                        $vip = $dadosVideo['vip'];
+                        switch($vip) {
+                            case 'SIM' : $cor = '#fc4d4d'; $corFonte = '#fff'; break;
+                            case 'NÃO' : $cor = '';  $corFonte = '' ; break;
+                        }
                         
+                        $inicio30 = date('H:i', strtotime('-30 minute', strtotime($dadosVideo['horaInicio'])));
+                        if ($inicio30 === date('H:i')) {
+                            echo "<embed src='../../assets/wav/sweetalertsound.wav' width='1' height='1'>";
+                            echo '<div id="alertaVideo"><p align="center">A PRÓXIMA VIDEOCONFERÊNCIA COMEÇA EM 30 MINUTOS!</p></div>';
+                        }
+
+                        $inicio15 = date('H:i', strtotime('-15 minute', strtotime($dadosVideo['horaInicio'])));                                  
+                        if ($inicio15 === date('H:i')) {
+                            echo "<embed src='../../assets/wav/sweetalertsound.wav' width='1' height='1'>";
+                            echo '<div id="alertaVideo"><p align="center">A PRÓXIMA VIDEOCONFERÊNCIA COMEÇA EM 15 MINUTOS!</p></div>';
+                        }
+                        
+            ?>
+                <tr style='background-color:<?php echo $cor;?>; color:<?php echo $corFonte;?>'>
+                    <td hidden ><?php echo $dadosVideo['id']; ?></td>
+                        <?php $dia = $dadosVideo['dia']; 
+                            $dia = date('d/m', strtotime($dia));
                         ?>
-                    </tbody>
-                </table>
-                <br>
-                 
-            </div>
-        </div>
+                    <td class="flow-text"><?php echo date('H:i', strtotime($dadosVideo['horaInicio'])); ?></td>
+                    <td class="flow-text"><?php echo date('H:i', strtotime($dadosVideo['horaFim'])); ?></td></b>
+                    <td class="flow-text"><?php echo $dadosVideo['ticket']; ?></td>
+                    <td class="flow-text"><?php echo $dadosVideo['pin']; ?></td>
+                    <td class="flow-text"><?php echo $dadosVideo['salasFisicas']; ?></td>
+                </tr>
+            <?php 
+                }  
+                    } else { ?>
+                    
+                    <tr>
+                        <td colspan="12"><div class="card-panel red center-align s12 text-lighten-1"><span style="color: #fff;">NENHUM REGISTRO ENCONTRADO</span></div></td>
+                    </tr>
+            <?php }  ?>
+        </tbody>
+    </table>
+    <br>
+</div>
 
     <script type="text/javascript">
-        setTimeout(function() {
-        $('#alertaVideo').fadeOut('fast');
-        }, 10000);
+        // setTimeout(function() {
+        // $('#alertaVideo').fadeOut('fast');
+        // }, 10000);
 	</script>
-<?php
 
+<?php
     //Rodapé
     include_once '../../app/inc/rodape.php';
 
